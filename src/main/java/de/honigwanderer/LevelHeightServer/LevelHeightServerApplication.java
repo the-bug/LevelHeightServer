c75a243a905287eb2cabb65ead021a60d1101064
the-bug <#w:1G7j~;*#2.BJc8QH>
2018-06-16 14:17:42 +0200

    added global cors

--- src/main/java/de/honigwanderer/LevelHeightServer/LevelHeightServerApplication.java
+++ src/main/java/de/honigwanderer/LevelHeightServer/LevelHeightServerApplication.java
@@ -2,6 +2,10 @@ package de.honigwanderer.LevelHeightServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class LevelHeightServerApplication {
@@ -9,4 +13,15 @@ public class LevelHeightServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LevelHeightServerApplication.class, args);
 	}
+	
+    @SuppressWarnings("deprecation")
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("*");
+            }
+        };
+    }
 }
