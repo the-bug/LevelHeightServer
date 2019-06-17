67e2a6b3045fa242ba60f0649bb7fb92dd739a0c
Jonas Scholz <jonas.scholz@die-honigwanderer.de>
2019-06-17 13:07:34 +0200

    added configtemplate

---
+++ sensor_client/configurations/configTemplate.h
@@ -0,0 +1,7 @@
+#define wLanSsid "Please Enter Your SSID"
+#define wLanPasswort "Please Enter Your Wlan Passwort"
+#define levelHeightServerLocationHardcoded "Please Enter Location of the Server"
+
+// May be skiped if levelHeightServerLocationHardcoded is hardcoded. TODO
+#define configurationRessourceHost "Please Enter Location of the configurationRessourceHost"
+#define configurationRessourceUri "Please Enter Location of the configurationRessourceUri"
